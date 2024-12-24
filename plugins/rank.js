bf88596f16c0031696869bf4469ad29b3613033a
KermRayan <kermd237@gmail.com>
2024-12-24 01:29:38 +0100

    Update rank.js

--- plugins/rank.js
+++ plugins/rank.js
@@ -52,7 +52,7 @@ cmd({
         const progressBar = "⭐".repeat(progressPercent / 10) + "⚪".repeat(10 - progressPercent / 10);
 
         // URL of the image for the rank
-        const levelImageURL = "https://imgur.com/a/8eV6kMk"; // Replace with your desired image URL
+        const levelImageURL = "https://files.catbox.moe/rrgoyh.jpeg"; // Replace with your desired image URL
         
         // Send rank information in text and image
         const caption = `📊 *Rank Information*\n\n👤 *User*: @${
