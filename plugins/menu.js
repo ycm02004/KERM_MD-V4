d37f4d3231107175d1adc6732b1dd7a91ec942b4
KermRayan <kermd237@gmail.com>
2024-12-27 22:46:25 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -45,6 +45,7 @@ let madeMenu = `╭━━━━∙⋆⋅⋆∙━ ─┉─ • ─┉─⊷
 *❖│▸* *ʀᴀᴍ ᴜsᴇ* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 *❖│▸* *ɴᴀᴍᴇ ʙᴏᴛ* : *❖『𝗞𝗘𝗥𝗠 𝗠𝗗 𝗩4』❖*
 *❖│▸* *ᴄʀᴇᴀᴛᴏʀ* : *➺Kɢ Tᴇᴄʜ࿐*
+*❖│▸* *ᴀʟᴡᴀʏs ᴏɴʟɪɴᴇ* : *[${config.ALWAYS_ONLINE}]*
 *❖│▸* *ᴠᴇʀsɪᴏɴs* : *ᴠ.4.1.0*
 *┬│▸*
 *└──────────────···▸*
