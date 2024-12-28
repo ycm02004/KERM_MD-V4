476efc1fa7754528401a7c024c15de8df0bf020b
KermRayan <kermd237@gmail.com>
2024-12-28 12:50:00 +0100

    Update nsfw.js

--- plugins/nsfw.js
+++ plugins/nsfw.js
@@ -1,38 +1,7 @@
 
 
 
-const { cmd } = require('../command'); // Assurez-vous que cmd est bien défini dans votre projet
 
-cmd({
-    pattern: "nsfw",
-    desc: "Display a list of NSFW options",
-    category: "fun",
-    use: '.nsfw',
-    react: "🔥",
-    filename: __filename
-},
-async (conn, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const nsfwList = `
-Here is your NSFW command, choose one:
-
-🍆 **PUSSY** 
-🍑 **FUCK**
-🔞 **HENTAI**
-
-Simply type the number corresponding to the option you'd like to choose.`;
-
-        // Send the NSFW list with an image and caption
-        await conn.sendMessage(from, { 
-            text: nsfwList, 
-            caption: 'Choose one from the list above!', 
-            image: { url: 'https://i.ibb.co/zFghN2T/Manul-Ofc-X.jpg' }
-        }, { quoted: mek });
-    } catch (e) {
-        console.error(e);
-        reply('❌ An error occurred while processing your request.');
-    }
-});
 cmd({
     pattern: "fuck", // Nom de la commande
     desc: "Fetch a NSFW image related to the command",
